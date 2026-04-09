5dbbb4e01b711aa746cb6bf0527ba745279ac8a4
borozco915 <borozco@dbu.edu>
2026-04-08 20:18:46 -0500

    Adding DockerFile, Package JSON, app file, task file, task.test file.

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let tasks = [];
+
+// Create Task
+app.post('/tasks', (req, res) => {
+    const task = { id: Date.now(), name: req.body.name };
+    tasks.push(task);
+    res.status(201).send(task);
+});
+
+// Get Tasks
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
+});
+
+// Update Task
+app.put('/tasks/:id', (req, res) => {
+    const task = tasks.find(t => t.id == req.params.id);
+    if (!task) return res.status(404).send("Not found");
+
+    task.name = req.body.name;
+    res.send(task);
+});
+
+// Delete Task
+app.delete('/tasks/:id', (req, res) => {
+    tasks = tasks.filter(t => t.id != req.params.id);
+    res.send("Deleted");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
