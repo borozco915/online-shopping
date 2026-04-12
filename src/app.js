ce64c1f31524d5641d4044ffe9c34ec7152e2c70
borozco915 <borozco@dbu.edu>
2026-04-12 00:30:08 -0500

    added a react js front end

--- src/app.js
+++ src/app.js
@@ -110,8 +110,8 @@ app.delete('/jerseys/:id', (req, res) => {
 
 // -------------------- SERVER --------------------
 
-const port = process.env.PORT || 8080;
+//const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-    console.log("⚽ Jersey Store running on port " + port);
-});
+//app.listen(port, () => {
+//    console.log("⚽ Jersey Store running on port " + port);
+//});
